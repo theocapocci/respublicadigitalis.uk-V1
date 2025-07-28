6a3adcbbe3e259eb9fa18495370d3ab495de0d9c
Theodore Brown <theodorecapocci@gmail.com>
2025-07-28 21:10:58 +0100

    Update config schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,28 @@
 // src/content/config.ts
 import { defineCollection, z } from 'astro:content';
 
+// Schema for the "notes" collection
 const notesSchema = z.object({
   title: z.string(),
-  description: z.string(),
+  description: z.string().optional(),
   image: z.string().optional(),
   tags: z.array(z.string()).optional(),
-  publish: z.boolean(),
-  date: z.date(),
+  publish: z.boolean().optional(),
+  date: z.date().optional(),
   uid: z.string().optional(),
 });
 
+// Schema for the "literature" collection
 const literatureSchema = z.object({
   title: z.string(),
-  description: z.string(),
+  description: z.string().optional(),
   image: z.string().optional(),
   tags: z.array(z.string()).optional(),
-  publish: z.boolean(),
-  date: z.date(),
+  publish: z.boolean().optional(),
+  date: z.date().optional(),
 });
 
+// Register the collections
 export const collections = {
   'notes': defineCollection({ schema: notesSchema }),
   'literature': defineCollection({ schema: literatureSchema }),
